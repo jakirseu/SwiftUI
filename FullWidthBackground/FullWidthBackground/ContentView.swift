f1d349a09f04eaec26b37f5786af85282a4844b1
Jakir Hossain <jakirseu@gmail.com>
2024-11-29 23:23:31 +0600

    image bg

--- FullWidthBackground/FullWidthBackground/ContentView.swift
+++ FullWidthBackground/FullWidthBackground/ContentView.swift
@@ -1,33 +1,29 @@
-//
-//  ContentView.swift
-//  FullWidthBackground
-//
-//  Created by Jakir Hossain on 29/11/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity) // Makes the VStack fill the entire screen
-        // Sets the background
-//        .background(Color.blue)
-        .background(
-            
+        ZStack{
             Image("background")
                 .resizable()
-                .scaledToFit()
-        )
-        
-        .ignoresSafeArea() // Ensures it extends beyond safe areas
-        
+                .scaledToFill()
+                .opacity(0.5)
+                .ignoresSafeArea()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            //        .frame(maxWidth: .infinity, maxHeight: .infinity)
+            //        .background(
+            //        Image("background")
+            //            .resizable()
+            //            .scaledToFill()
+            //            .opacity(0.5)
+            //            .ignoresSafeArea()
+            //        )
+            
+        }
     }
 }
 
